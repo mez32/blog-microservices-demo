1740e69f96c5cd27621e0e7f802fc0acf5b16548
mez32 <amesmer32@gmail.com>
2022-09-06 13:54:59 -0400

    Finished. Added docker and kubernetes files to project

--- comments/index.js
+++ comments/index.js
@@ -26,7 +26,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 	commentsByPostId[id] = comments
 
-	await axios.post('http://localhost:4005/events', {
+	await axios.post('http://event-bus-srv:4005/events', {
 		type: 'CommentCreated',
 		data: {
 			postId: id,
@@ -50,7 +50,7 @@ app.post('/events', async (req, res) => {
 		const comment = commentsByPostId[postId].find((comment) => comment.id === id)
 		comment.status = status
 
-		await axios.post('http://localhost:4005/events', {
+		await axios.post('http://event-bus-srv:4005/events', {
 			type: 'CommentUpdated',
 			data: {
 				...data,
